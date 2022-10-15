9b5f363ce6eb257efe52fd0cb6e8218b934f3c3e
Agil Triono <agiltriono71@yahoo.com>
2022-10-16 05:54:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, Intents, Collection } = require("discord.js");
 const Discord = require("discord.js");
+const express = require("express")
 const { DISCORD_TOKEN, numformat, timeconvert, genId, database, PREFIX } = require("./util/util");
 const client = new Client({ intents : ["GUILDS","GUILD_BANS","GUILD_MEMBERS","GUILD_MESSAGES","GUILD_MESSAGE_TYPING","GUILD_MESSAGE_REACTIONS","DIRECT_MESSAGES","DIRECT_MESSAGE_REACTIONS","DIRECT_MESSAGE_TYPING","GUILD_WEBHOOKS","GUILD_EMOJIS_AND_STICKERS"],
   partials: ['MESSAGE', 'CHANNEL', 'REACTION']
@@ -19,6 +20,7 @@ client.cooldowns = new client.discord.Collection();
 client.votesdown = new client.discord.Collection();
 client.game = [];
 client.login(DISCORD_TOKEN);
+express().listen(port, () => console.log(`Server ready, Listening on ${port}`));
 const eventFiles = fs.readdirSync('./event').filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
