e6ecc0ac419497a6d1dbefd47a0b0262c072e270
Agil Triono <agiltriono71@yahoo.com>
2022-10-16 06:55:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ for (const file of eventFiles) {
 // Logging
 client.on("warn", (info) => console.log(info));
 client.on("error", (error) =>  console.error(error));
+
 process.on('unhandledRejection', error => {
 	console.error('Unhandled promise rejection:', error);
 });
